fa7cde45bc7a61cfaff5acbc7437d27e6120afc0
gavmc23 <373020@guhsd.net>
2026-05-29 07:40:55 -0700

    Trying to update vite config with repo name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Papas cheat oria/',
+  base: '/mr-pyle-react-template-w7hb3vp1/',
 })
